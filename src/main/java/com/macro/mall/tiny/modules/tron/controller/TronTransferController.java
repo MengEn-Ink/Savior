139089aae40be09a3d365f37bab32ba133e1f8d8
Brain <MengEn4j@gmail.com>
2020-11-11 00:34:50 +0800

    add ...

--- src/main/java/com/macro/mall/tiny/modules/tron/controller/TronTransferController.java
+++ src/main/java/com/macro/mall/tiny/modules/tron/controller/TronTransferController.java
@@ -6,8 +6,8 @@ import com.macro.mall.tiny.modules.tron.model.TronTransfer;
 import com.macro.mall.tiny.modules.tron.service.TronTransferService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +29,7 @@ public class TronTransferController {
     private TronTransferService tronTransferService;
 
     @ApiOperation("获取所有交易")
-    @RequestMapping(value = "/listAll", method = RequestMethod.GET)
+    @GetMapping(value = "/listAll")
     @ResponseBody
     public CommonResult<List<TronTransfer>> listAll() {
         List<TronTransfer> roleList = tronTransferService.list();
